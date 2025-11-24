25c8e226464f1370fb6b6bfebfca1b23a90193a5
unknown <abhinashm606@gmail.com>
2025-11-25 01:34:15 +0530

    first commit

--- Copy.java
+++ Copy.java
@@ -1,14 +1,47 @@
+// class Copy
+// {
+//     int a;
+//     Copy(int a)
+//     {
+//         this.a=a;
+//         System.out.println(a);
+//     }
+
+// public static void main(String args[])  
+// {
+//     Copy obj=new Copy(11);
+// }  
+// }
+
 class Copy
 {
-    int a;
-    Copy(int a)
+    String name[];
+
+    Copy(String name[])
     {
-        this.a=a;
-        System.out.println(a);
+        this.name=name;
     }
 
-public static void main(String args[])  
+Copy(Copy obj) 
 {
-    Copy obj=new Copy(11);
-}  
+this.name=new String[obj.name.length];
+
+for(String name:obj.name)
+{
+
 }
+}
+
+public static void main(String args[])
+{
+    String name[]={"abhi","mishra"};
+    Copy obj1=new Copy(name);
+    Copy obj2=new Copy(obj1);
+
+    obj2.name[0]="avinash";
+    System.out.println(obj1.name[0]);
+}
+}
+
+
+
