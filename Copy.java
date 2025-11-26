3cc2e658da2a87d78fb2856bb64eb5dcbf6fef02
unknown <abhinashm606@gmail.com>
2025-11-26 21:59:31 +0530

    first commit

--- Copy.java
+++ Copy.java
@@ -13,35 +13,63 @@
 // }  
 // }
 
-class Copy
+// class Copy
+// {
+//     String name[];
+
+//     Copy(String name[])
+//     {
+//         this.name=name;
+//     }
+
+// Copy(Copy obj) 
+// {
+// this.name=new String[obj.name.length];
+
+// for(String name:obj.name)
+// {
+
+// }
+// }
+
+// public static void main(String args[])
+// {
+//     String name[]={"abhi","mishra"};
+//     Copy obj1=new Copy(name);
+//     Copy obj2=new Copy(obj1);
+
+//     obj2.name[0]="avinash";
+//     System.out.println(obj1.name[0]);
+// }
+// }
+
+class Copy{
+    
+}
+public  static void main (String args[])
 {
-    String name[];
+try
+   {
+    int a=10;
+    int b=0;
+    System.out.println(a/b);
+}
 
-    Copy(String name[])
-    {
-        this.name=name;
-    }
-
-Copy(Copy obj) 
+                                    
+catch(ArithmeticException obj)
 {
-this.name=new String[obj.name.length];
-
-for(String name:obj.name)
-{
-
+System.out.println("jnfeknfk");
 }
 }
 
-public static void main(String args[])
-{
-    String name[]={"abhi","mishra"};
-    Copy obj1=new Copy(name);
-    Copy obj2=new Copy(obj1);
 
-    obj2.name[0]="avinash";
-    System.out.println(obj1.name[0]);
-}
-}
+
+
+
+
+
+
+
 
 
 
