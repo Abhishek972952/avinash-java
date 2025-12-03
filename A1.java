f4638c4b6aaa29623825b3723a20451d32f2dcec
unknown <abhinashm606@gmail.com>
2025-12-03 12:40:01 +0530

    first commit

--- A1.java
+++ A1.java
@@ -1,27 +1,29 @@
 import java.util.Scanner;
 class A1{
     public static void main(String args[])
-    {
-        Scanner obj=new Scanner(System.in);
-        int a=10;
-        int b=0;
+{
+Scanner obj=new Scanner(System.in);
+int a=10;
+int b=0;
 
-try{
-    System.out.println(a/b);
+try
+{
+System.out.println(a/b);
 }  
 
 catch(Exception obj1)
 {
-System.out.println(a/b);
+System.out.println("abhishek mishra");
 System.out.println(obj1);
 
 }
 
 finally
 {
-    obj.close();
+obj.close();
 }
-    }
+
+}
 }
 
 
